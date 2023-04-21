eed493f39306cf39ca7b947c629c0b87ecfb0ced
Manish Kesharwani <112785474+man9936@users.noreply.github.com>
2023-04-21 22:30:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 
 const app=express();
-const port =3000;
+const port =4000;
 
 
 
 app.get('/',(req,res)=>{
-    res.send('Hello World!');
+    res.send('Print Manish kesharwani!');
 })
 
 app.listen(port,(req,res,next)=>{
     console.log('listening on '+ port);
 })
+
+const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+
+let x= array.map((item)=>{
+   if(item ==' '){return "empty string"}
+   else return;
+})
+console.log(x);
